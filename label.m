4a7a738dbf9c4d81900ddd012c6a8f313d184fe8
premsagar279 <abcpremsagar@gmail.com>
2017-08-09 15:01:17 +0530

    Update label.m

--- label.m
+++ label.m
@@ -1,11 +1,20 @@
-function label( result,v )
+function label( X,result,v,V)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-n=length(v)-1;
+C = rand(v,1);%for choosing random color for node
+k = 1:v;
+hold on
+scatter(V(:,1), V(:,2), [],C,'filled');
+gplot(X(k,k),V(k,:),'-or');
+text(V(:,1), V(:,2),[repmat('  ',v,1), num2str((1:v)')]);
+hold off
+n=v-1;
 for i=1:n
-    x=(v(result(i).src,1)+v(result(i).dest,1))/2;
-    y=(v(result(i).src,2)+v(result(i).dest,2))/2;
-    w=result(i).weight;
-    text(x,y,w);
+    x=(V(result(i).src,1)+V(result(i).dest,1))/2;
+    y=(V(result(i).src,2)+V(result(i).dest,2))/2;
+    w=num2str(result(i).weight);
+    t=['\leftarrow',w];
+    text(x,y,t);
+end
 end
 
