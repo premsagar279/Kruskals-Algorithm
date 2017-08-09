12a68532d89631c0d394adc8d4cb29fa9ac1d0cb
premsagar279 <abcpremsagar@gmail.com>
2017-08-09 15:02:09 +0530

    Update kruskals_main.m

--- kruskals_main.m
+++ kruskals_main.m
@@ -6,7 +6,11 @@ e=input('no of edges:');
 %returns a graph having source destination and weight of crossponding edges
 graph=gin(e);
 fprintf('enter coordinates of %d vertices in order\n',v);
-V=ginput(v);  %V is vx2 matrix containing coordinate of each v vertices
+V=ginput(v);
+%display input graph
+%Y=disp_mst(graph,v);
+%label(Y,graph,v,V);
+%V is vx2 matrix containing coordinate of each v vertices
 %sorting the edge of graph crossponding to its weight and storing its value
 %in new graph graph g
 g=gsort(graph,'weight');
@@ -42,21 +46,7 @@ disp('Following are the edges in the constructed MST');
 X=disp_mst(result,v); %X is adjecency matrix of mst
 %disp(X);
 %PLLOTING THE REQUIRED MST
-C = rand(v,1);%for choosing random color for node
-k = 1:v;
-hold on
-scatter(V(:,1), V(:,2), [],C,'filled');
-gplot(X(k,k),V(k,:),'-or');
-text(V(:,1), V(:,2),[repmat('  ',v,1), num2str((1:v)')]);
-hold off
-n=v-1;
-for i=1:n
-    x=(V(result(i).src,1)+V(result(i).dest,1))/2;
-    y=(V(result(i).src,2)+V(result(i).dest,2))/2;
-    w=num2str(result(i).weight);
-    t=['\leftarrow',w];
-    text(x,y,t);
-end
+label(X,result,v,V);
      
 
 
